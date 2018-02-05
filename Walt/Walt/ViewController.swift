7e9e97de8493d90edb40cb1d8d8ed904e1f74f32
Hayat AlHassan <hah308@nyu.edu>
2018-02-05 19:14:05 +0400

    created xcode project

---
+++ Walt/Walt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Walt
+//
+//  Created by Abdulredha Abu al hassan on 2/5/18.
+//  Copyright © 2018 Walturn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
